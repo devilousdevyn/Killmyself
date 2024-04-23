d6668a08fbd17f00d912976a1a514d374001b0e5
tomek <wojs.tomek@gmail.com>
2024-04-23 20:13:41 +0200

    nie obchodzi mnie to kys

--- Killingmyself/Main.java
+++ Killingmyself/Main.java
@@ -1,5 +1,7 @@
 package Killingmyself;
 
+import java.util.Scanner;
+
 class Main {
 
 	public static void main(String[] args) {
@@ -16,6 +18,51 @@ class Main {
 		 map[7]=new Places("Boss");
 		 map[8]=new Places("8");
 		 map[9]=new Places("9");
-	}
-
-}
+		 
+//		 		map[0].dodajPrzejscie(null, mapa[1], null, null);
+//		 		map[1].dodajPrzejscie(mapa[3], mapa[2], null, mapa[0]);
+//		 		map[2].dodajPrzejscie(null, null, null, mapa[1]);
+//		 		map[3].dodajPrzejscie(mapa[4], null, mapa[1], null);
+//		 		map[4].dodajPrzejscie(mapa[5], mapa[6], mapa[3], null);
+//		 		map[5].dodajPrzejscie(null, null, mapa[4], null);
+//		 		map[6].dodajPrzejscie(null, null, null, mapa[4]);
+//		 		
+		 		Places aktualneMiejsce = map[0];
+		 		Scanner scan = new Scanner(System.in);
+		 		char wybor;
+		 		while(!aktualneMiejsce.equals(map[6])) {
+		 			System.out.println("Jesteś w "+ aktualneMiejsce.getNazwa());
+		 			System.out.println("Możesz pójść: " + aktualneMiejsce.wypiszPrzejscia());
+		 		}
+		 		
+		 		do {
+		 			System.out.println("Twój wybór: ");
+		 			wybor = scan.next().charAt(0);
+		 		} while(!(wybor=='w' || wybor=='d' || wybor=='s' || wybor=='a' ));
+		 		
+		 		int i=0;
+		 		switch(wybor) {
+		 		case 'w':
+		 			i=0;
+		 			break;
+		 		case 'd':
+		 			i=1;
+		 			break;
+		 		case 's':
+		 			i=2;
+		 			break;
+		 		case 'a':
+		 			i=3;
+		 			break;
+		 		};
+		 		
+		 		if(aktualneMiejsce.wybierzPrzejscie(i)!=null)
+		 			aktualneMiejsce=aktualneMiejsce.wybierzPrzejscie(i);
+		 		else
+		 			System.out.println("Nieprawidłowy Wybór");
+		 	
+		 		System.out.println("``You're not supposed to be here.`` said God after entering his garden unannounced");
+		 		System.out.println("YOU DIED");
+		 	}
+		 	
+		 }
