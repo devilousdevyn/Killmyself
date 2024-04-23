9d9aed0b99e5eb70eefcc7e3b5c09beebff52c60
tomek <wojs.tomek@gmail.com>
2024-04-23 20:08:04 +0200

    pierwszy. comit.

---
+++ Killingmyself/Main.java
@@ -0,0 +1,21 @@
+package Killingmyself;
+
+class Main {
+
+	public static void main(String[] args) {
+
+		 Places[] map = new Places[10];
+		 
+		 map[0]=new Places("Start");
+		 map[1]=new Places("1");
+		 map[2]=new Places("2");
+		 map[3]=new Places("3");
+		 map[4]=new Places("4");
+		 map[5]=new Places("5");
+		 map[6]=new Places("6");
+		 map[7]=new Places("Boss");
+		 map[8]=new Places("8");
+		 map[9]=new Places("9");
+	}
+
+}
---
+++ Killingmyself/Places.java
@@ -0,0 +1,44 @@
+package Killingmyself;
+
+public class Places {
+
+	protected String nazwa;
+	protected String opis;
+	protected Places[] przejscia = new Places[4];
+	
+	public Places(String nazwa) {
+		this.nazwa=nazwa;
+		}
+	public void dodajPrzejscie(Places w, Places a, Places s, Places d) {
+		przejscia[0]=w;
+		przejscia[1]=a;
+		przejscia[2]=s;
+		przejscia[3]=d;
+	}
+	
+	public String wypiszPrzejscia() {
+		String drzwi = "";
+		if(przejscia[0]!=null) drzwi+="[w]góra";
+		if(przejscia[1]!=null) drzwi+="[a]lewo";
+		if(przejscia[2]!=null) drzwi+="[s]dół";
+		if(przejscia[3]!=null) drzwi+="[d]prawo";
+		
+		return drzwi;
+	}
+	
+	public Places wybierzPrzejscie(int i) {
+		return przejscia[i];
+	}
+	public String getNazwa() {
+		return nazwa;
+	}
+	public void setNazwa(String nazwa) {
+		this.nazwa = nazwa;
+	}
+	public String getOpis() {
+		return opis;
+	}
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+}
